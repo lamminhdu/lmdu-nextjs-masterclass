bcfe853e5b9b0b56ae9fc16deaa7ae9e7deb5008
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-07 15:29:10 +0000

    tutorial-5: update code follow tutorial

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
 // components
 import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,13 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        {/* <nav>
-          <h1>Helpdesk</h1>
-          <Link href="/">Dashboard</Link>
-          <Link href="/tickets">Tickets</Link>
-        </nav> */}
+      <body className={rubik.className}>
         <Navbar />
-      <body className={inter.className}>{children}</body>
+        {children}
+      </body>
     </html>
   )
 }
