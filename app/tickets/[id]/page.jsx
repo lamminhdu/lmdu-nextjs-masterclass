18aaef1e4866b6890308a02ff024db347f7ca370
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-08 15:53:18 +0000

    tutorial-8: static rendering

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,6 +1,20 @@
+// export const dynamicParams = false
+import { notFound } from 'next/navigation'
+export async function generateStaticParams() {
+    const res = await fetch("http://localhost:4000/tickets")
+
+    const tickets = await res.json()
+
+    return tickets.map(ticket => ({id: ticket.id}))
+}
+
 async function getTicket(id) {
     const res = await fetch("http://localhost:4000/tickets/" + id, {next: {revalidate: 60}})
     console.log(res)
+    if (!res.ok) {
+        notFound()
+    }
+    
     return res.json()
 }
 
