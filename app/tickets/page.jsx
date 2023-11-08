1c3042d26b8e9fce999ad7e664c38a4f26d3e974
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-08 16:53:10 +0000

    tutorial-10: loading and suspense

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import TicketList from './TicketList'
+import Loading from '../loading'
 
 export default function Tickets() {
   return (
@@ -10,7 +11,10 @@ export default function Tickets() {
           <p><small>Currently open tickets</small></p>
         </div>
       </nav>
-      <TicketList />
+      
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   )
 }
