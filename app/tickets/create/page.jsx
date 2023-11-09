3051288360cf536c3e9d7e715c6474d7714bee74
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-09 16:03:52 +0000

    tutorial-11: client form component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TicketForm from './TicketForm'
+
+export default function CreateTicket() {
+  return (
+    <main>
+        <h2 className='text-primary text-center'>Add a new ticket</h2>
+        <TicketForm />
+    </main>
+  )
+}
